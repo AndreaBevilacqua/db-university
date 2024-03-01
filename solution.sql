9353d8dff78c958e3220c275ef31e2224a80bc8d
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-01 17:24:44 +0100

    added 6-7 task

--- solution.sql
+++ solution.sql
@@ -16,7 +16,13 @@ SELECT `name`, `period`, `year` FROM `courses` WHERE `period` = 'I semestre' AND
 SELECT * FROM `exams` WHERE DATE(`date`) = '2020-06-20' AND TIME(`hour`) > '14:00';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = `magistrale`;
+
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) FROM `departments`;
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 -- ? ESERCIZI CON GROUP BY
