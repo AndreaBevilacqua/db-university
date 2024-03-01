a9db691ab895a27acc28ac964b74e5727fc18d36
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-01 17:28:31 +0100

    added 1 group by task

--- solution.sql
+++ solution.sql
@@ -29,6 +29,8 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 -- ? ESERCIZI CON GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT `enrolment_date`, COUNT(*) FROM `students` GROUP BY `enrolment_date`;
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
