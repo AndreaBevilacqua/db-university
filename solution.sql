bd56df0d6a8d2dc54a4f3850b4dfde8de510aed0
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-04 17:00:51 +0100

    added 4 join task

--- solution.sql
+++ solution.sql
@@ -52,6 +52,8 @@ SELECT C.`id`, C.`name`,D.`name` FROM `courses` AS C JOIN `departments` AS D ON 
 SELECT * FROM `courses` AS C JOIN `teachers` AS T ON C.`id` = T.id WHERE T.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT * FROM `students` AS S INNER JOIN `degrees` AS D ON S.`id` = D.`id` INNER JOIN `departments` AS P on D.`id` = P.`id` ORDER BY S.`surname`;
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
