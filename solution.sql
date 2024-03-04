4aa053c6c8207bc8c5af1355cf48be486065d24d
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-04 16:53:18 +0100

    added 3 join task

--- solution.sql
+++ solution.sql
@@ -49,6 +49,8 @@ SELECT S.`id`, S.`name`,D.`name` FROM `students` AS S JOIN `degrees` AS D ON S.`
 SELECT C.`id`, C.`name`,D.`name` FROM `courses` AS C JOIN `departments` AS D ON C.`id` = D.id WHERE D.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * FROM `courses` AS C JOIN `teachers` AS T ON C.`id` = T.id WHERE T.`id` = 44;
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
